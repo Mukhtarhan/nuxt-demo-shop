0e4caa464ed4751280859430f2877a3d497347d5
Erkebulan <kalkadamov14@gmail.com>
2024-12-14 16:07:59 +0500

    Сохранение изменений в app.vue

--- app.vue
+++ app.vue
@@ -5,51 +5,51 @@ import Drawer from "./components/Drawer.vue";
 const title = ref("Nuxt Shop");
 const description = ref("A simple shop built with Nuxt.");
 useHead({
-  titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - Nuxt Shop` : "Nuxt Shop";
-  },
-  meta: [
-    //Open Graph
-    {
-      key: "og-type",
-      property: "og:type",
-      content: "website",
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} - Nuxt Shop` : "Nuxt Shop";
     },
-    {
-      key: "og-url",
-      property: "og:url",
-      content: `https://anuxtshop.netlify.app/`,
-    },
-    //Twitter
-    {
-      key: "twitter-card",
-      property: "twitter:card",
-      content: "summary_large_image",
-    },
-    {
-      key: "twitter-url",
-      property: "twitter:url",
-      content: `https://anuxtshop.netlify.app/`,
-    },
-  ],
+    meta: [
+        //Open Graph
+        {
+            key: "og-type",
+            property: "og:type",
+            content: "website",
+        },
+        {
+            key: "og-url",
+            property: "og:url",
+            content: `https://anuxtshop.netlify.app/`,
+        },
+        //Twitter
+        {
+            key: "twitter-card",
+            property: "twitter:card",
+            content: "summary_large_image",
+        },
+        {
+            key: "twitter-url",
+            property: "twitter:url",
+            content: `https://anuxtshop.netlify.app/`,
+        },
+    ],
 });
 
 useSeoMeta({
-  description: description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: `https://anuxtshop.netlify.app/assets/images/cover.jpg`,
-  twitterTitle: title,
-  twitterDescription: description,
-  twitterImage: `https://anuxtshop.netlify.app/assets/images/cover.jpg`,
+    description: description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: `https://anuxtshop.netlify.app/assets/images/cover.jpg`,
+    twitterTitle: title,
+    twitterDescription: description,
+    twitterImage: `https://anuxtshop.netlify.app/assets/images/cover.jpg`,
 });
 </script>
 <template>
-  <NuxtLayout>
-    <div class="bg-white max-w-7xl mx-auto mt-14">
-      <NuxtLoadingIndicator />
-      <NuxtPage />
-      <!-- <Drawer /> -->
-    </div>
-  </NuxtLayout>
+    <NuxtLayout>
+        <!-- <div class="bg-white max-w-7xl mx-auto mt-14"> -->
+        <NuxtLoadingIndicator />
+        <NuxtPage />
+        <!-- <Drawer /> -->
+        <!-- </div> -->
+    </NuxtLayout>
 </template>
